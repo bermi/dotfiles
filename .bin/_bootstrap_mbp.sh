4fd3dc24784c72b69e2e2cdf2aa060f110e58597
Bermi Ferrer <bermi@bermilabs.com>
2023-03-21 03:57:47 +0100

    Adding example configs

--- .bin/_bootstrap_mbp.sh
+++ .bin/_bootstrap_mbp.sh
@@ -20,6 +20,7 @@ brew install --cask \
 brew install \
   ack \
   bash \
+  direnv \
   git \
   nvim \
   multipass \
@@ -29,4 +30,14 @@ brew install \
   the-unarchiver \
   trash \
   wget
+
+sh <(curl -L https://nixos.org/nix/install)
+
+nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+nix-channel --add https://github.com/LnL7/nix-darwin/archive/master.tar.gz darwin
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Bermi Ferrer dotfiles
 
 This repo includes my setup as of 2023.
+
+The file `.bin/_bootstrap_mbp.sh` includes some of the commands that
+can be used to define a new dev environment from scratch.
+
+Followed the tutorial on https://www.youtube.com/watch?v=CF1tMjvHDRA to
+setup an environment using instructions relevant in 2023
